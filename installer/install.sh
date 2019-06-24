7b7f8816f5cc3875bea026a4fe2cc26a8dec28ad
Rodolphe Pineau <pineau@rti-zone.org>
2019-06-23 17:51:20 -0700

    collapse install script for linux and arm

--- installer/install.sh
+++ installer/install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,14 +22,24 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else [ -d "$TheSkyX_Path/Resources/Common/PlugInsARM32" ]; then
+	PLUGINS_DIR="PlugInsARM32"
+else [ -d "$TheSkyX_Path/Resources/Common/PlugInsARM64" ]; then
+	PLUGINS_DIR="PlugInsARM32"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./focuserlist aaf2.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./aaf2.ui" "$TheSkyX_Path/Resources/Common/PlugInsARM32/FocuserPlugIns/"
-cp "./libaaf2.so" "$TheSkyX_Path/Resources/Common/PlugInsARM32/FocuserPlugIns/"
+cp "./aaf2.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
+cp "./libaaf2.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/focuserlist aaf2.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/FocuserPlugIns/aaf2.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/FocuserPlugIns/libaaf2.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/aaf2.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libaaf2.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugInsARM32/FocuserPlugIns/libaaf2.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libaaf2.so"
